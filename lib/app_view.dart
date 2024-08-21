6a7a69ed5f69c127f24f0ca0a9d186cf07e35d26
merinda <melrylimace@gmail.com>
2024-08-21 17:20:58 -0300

    add WelcomeScreen com navegação por guias e integração de bloco de autenticação

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,5 +1,9 @@
+import 'package:app_ecommerce/blocs/authentication_bloc/authentication_bloc.dart';
+import 'package:app_ecommerce/screens/home/views/home_screen.dart';
+import 'package:app_ecommerce/screens/home/views/welcome_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyAppView extends StatelessWidget {
   const MyAppView({super.key});
@@ -8,6 +12,25 @@ class MyAppView extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Eco Shop',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          background: Colors.grey.shade100,
+          onBackground: Colors.black,
+          primary: Colors.blue,
+          onPrimary: Colors.white
+        )
+      ),
+      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+        builder: ((context, state){
+          if(state.status == AuthenticationStatus.authenticated){
+            return HomeScreen();
+          } else {
+            return WelcomeScreen();
+          }
+        }
+        )
+      ),
     );
   }
 }
